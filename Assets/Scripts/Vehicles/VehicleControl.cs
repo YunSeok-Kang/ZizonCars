d501885300629fbed4b0d0f7faace806b122c0dd
YunSeok-Kang <ysck73@naver.com>
2017-10-24 16:17:23 +0900

    VehicleControl 스크립트 작성 및 F1 차량에 적용. - 직진 기능 - 좌우 회전 기능

---
+++ Assets/Scripts/Vehicles/VehicleControl.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VehicleControl : MonoBehaviour {
+
+    private Rigidbody vehicleRigidbody = null;
+
+    public float accerationForce = 0.0f;
+    public float rotationRatio = 0.0f;
+
+	// Use this for initialization
+	void Start () {
+        if (vehicleRigidbody == null)
+            vehicleRigidbody = gameObject.GetComponent<Rigidbody>();
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        Vector3 currentRotation = transform.localRotation.eulerAngles;
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            currentRotation.y -= (rotationRatio * Time.deltaTime);
+        }
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            currentRotation.y += (rotationRatio * Time.deltaTime);
+        }
+        transform.localRotation = Quaternion.Euler(currentRotation);
+	}
+
+    void FixedUpdate()
+    {
+        vehicleRigidbody.AddRelativeForce(Vector3.forward * accerationForce);
+    }
+}
