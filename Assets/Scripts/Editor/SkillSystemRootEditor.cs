0b8a679e545e80efc6e9d6f1719db02104210c83
YunSeok-Kang <ysck73@naver.com>
2017-10-26 16:35:51 +0900

    *스킬 시스템 Root 제작 * 스킬 시스템 Editor 제작     - VehicleControl 클래스가 붙어있지 않은 GameObject에 SkillsystemRoot가 추가되려 하는 경우에 경고 메시지를 띄우고 추가 행위 취소

---
+++ Assets/Scripts/Editor/SkillSystemRootEditor.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+
+[CustomEditor(typeof(SkillSystemRoot))]
+public class SkillSystemRootEditor : Editor
+{
+    void OnEnable()
+    {
+        SkillSystemRoot targetComponent = target as SkillSystemRoot;
+        VehicleControl vehicleControl = targetComponent.gameObject.GetComponent<VehicleControl>();
+        if (vehicleControl == null)
+        {
+            Debug.LogError("해당 스크립트가 Attach 되기 위해서는 VehicleControl 스크립트가 존재하여야 합니다.");
+            DestroyImmediate(targetComponent);
+        }
+    }
+}
---
+++ Assets/Scripts/Skills/SkillSystemRoot.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SkillUserInfo
+{
+    // 사용 주체
+    public GameObject gameObject;
+}
+
+public class SkillSystemRoot : MonoBehaviour {
+
+    SkillUserInfo skillUserInfo = null;
+
+    protected bool Init()
+    {
+        SkillUserInfo userInfo = new SkillUserInfo();
+        userInfo.gameObject = gameObject;
+        // Todo : SkillUserInfo 클래스에 필드가 추가되면 해당 위치에서 초기화 시켜줄 것.
+
+        skillUserInfo = userInfo;
+        return true;
+    }
+
+    public virtual void UseSkill()
+    {
+
+    }
+}
