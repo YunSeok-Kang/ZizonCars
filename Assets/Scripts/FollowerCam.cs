b0022306f10dc173bc35d86edf72108335bf182c
YunSeok-Kang <ysck73@naver.com>
2017-10-31 16:14:10 +0900

    - FollowerCam 스크립트 추가 : Target을 카메라가 쫓아가게 됨. - FollowerCam 프리팹 추가.

---
+++ Assets/Scripts/FollowerCam.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowerCam : MonoBehaviour {
+
+    public Transform followingTarget = null;
+
+    public bool followPosX = false;
+    public bool followPosY = false;
+    public bool followPosZ = false;
+
+    // Use this for initialization
+    void Start () {
+        if (followingTarget == null)
+            Debug.Log("FollowingTarget is null.");
+	}
+	
+	// Update is called once per frame
+	void LateUpdate () {
+
+        Vector3 curCamPos = transform.position;
+
+		if (followPosX)
+        {
+            curCamPos.x = followingTarget.position.x;
+        }
+
+        if (followPosY)
+        {
+            curCamPos.y = followingTarget.position.y;
+        }
+
+        if (followPosZ)
+        {
+            curCamPos.z = followingTarget.position.z;
+        }
+
+        transform.position = curCamPos;
+	}
+}
