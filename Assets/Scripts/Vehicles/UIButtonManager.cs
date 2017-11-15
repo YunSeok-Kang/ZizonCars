a214dc5d388e450ae401d522b1e39280c45dfb21
Doeun <doeuncow@naver.com>
2017-11-15 11:53:26 +0900

    IGUI 모바일용 버튼 스크립트 제작 중

---
+++ Assets/Scripts/Vehicles/UIButtonManager.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UIButtonManager : MonoBehaviour {
+
+	// Use this for initialization
+	void LeftClickt () {
+		
+	}
+	
+	// Update is called once per frame
+	void RightClick () {
+		
+	}
+
+    void SkillClick()
+    {
+        Debug.Log('Skill');
+    }
+}
