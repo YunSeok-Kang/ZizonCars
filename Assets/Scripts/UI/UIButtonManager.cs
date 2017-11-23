7e691d228d87160864145f7a34f54a3e0890fe03
Doeun <doeuncow@naver.com>
2017-11-23 16:23:48 +0900

    Revert "Revert "UIButtonManager 스크립트 1차 제작""
    This reverts commit 986a614dbb5fb428b97559fcca7aec11b8d2d1e6.

---
+++ Assets/Scripts/UI/UIButtonManager.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UIButtonManager : MonoBehaviour
+{
+    public void LeftClick()
+    {
+
+    }
+    public void RightClick()
+    {
+
+    }
+    public void SkillClick()
+    {
+        Debug.Log("Skill");
+    }
+}
