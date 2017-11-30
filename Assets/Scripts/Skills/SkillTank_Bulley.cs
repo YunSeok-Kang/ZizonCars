08386de7dae443b49433288bf4efb652d6488097
Ura7536 <zxcv123594@gmail.com>
2017-11-30 17:58:20 +0900

    탱크 스킬 구현 완료

---
+++ Assets/Scripts/Skills/SkillTank_Bulley.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SkillTank_Bulley : MonoBehaviour {
+
+    private Rigidbody vehicleRigidbody = null;
+    public float bulletSpeed = 0.0f;
+
+	// Use this for initialization
+	void Start () {
+        vehicleRigidbody.AddForce(Vector3.forward * bulletSpeed);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
